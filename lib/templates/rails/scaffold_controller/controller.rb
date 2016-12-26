7852f9cdf5fc575e90c1056d1e01383df96ff5c3
Ian Ooi <ianooism@gmail.com>
2016-12-26 14:30:06 +0000

    updated scaffold_controller

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -5,54 +5,53 @@ require_dependency "<%= namespaced_path %>/application_controller"
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
   def index
-    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+    render :index, locals: { <%= plural_table_name %>: <%= orm_class.all(class_name) %> }
   end
-
+  
   def show
-    @<%= singular_table_name %> = set_<%= singular_table_name %>
+    render :show, locals: { <%= singular_table_name %>: current_<%= singular_table_name %> }
   end
-
+  
   def new
-    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    render :new, locals: { <%= singular_table_name %>: <%= orm_class.build(class_name, "new_#{singular_table_name}_params") %> }
   end
-
+  
   def create
-    @<%= singular_table_name %> = <%= orm_class.build(class_name, "form_params") %>
+    new_<%= singular_table_name %> = <%= orm_class.build(class_name, "new_#{singular_table_name}_params") %>
     
-    if @<%= orm_instance.save %>
-      redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} created.'" %>
+    if new_<%= singular_table_name %>.update(<%= "#{singular_table_name}_form_params" %>)
+      redirect_to <%= index_helper %>_url, notice: <%= %Q("#{human_name} created.") %>
     else
-      render 'new'
+      render :new, locals: { <%= singular_table_name %>: new_<%= singular_table_name %> }
     end
   end
   
   def edit
-    @<%= singular_table_name %> = set_<%= singular_table_name %>
+    render :edit, locals: { <%= singular_table_name %>: current_<%= singular_table_name %> }
   end
-
+  
   def update
-    @<%= singular_table_name %> = set_<%= singular_table_name %>
-    
-    if @<%= orm_instance.update("form_params") %>
-      redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} updated.'" %>
+    if current_<%= singular_table_name %>.update(<%= "#{singular_table_name}_form_params" %>)
+      redirect_to <%= index_helper %>_url, notice: <%= %Q("#{human_name} updated.") %>
     else
-      render 'edit'
+      render :edit, locals: { <%= singular_table_name %>: current_<%= singular_table_name %> }
     end
   end
-
+  
   def destroy
-    @<%= singular_table_name %> = set_<%= singular_table_name %>
-    
-    @<%= orm_instance.destroy %>
-    redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} destroyed.'" %>
+    current_<%= singular_table_name %>.destroy
+    redirect_to <%= index_helper %>_url, notice: <%= %Q("#{human_name} destroyed.") %>
   end
-
+  
   private
-    def set_<%= singular_table_name %>
-      <%= orm_class.find(class_name, "params[:id]") %>
+    def current_<%= singular_table_name %>
+      @<%= singular_table_name %> ||= <%= orm_class.find(class_name, "params[:id]") %>
     end
-
-    def <%= "form_params" %>
+    
+    def <%= "new_#{singular_table_name}_params" %>
+    end
+    
+    def <%= "#{singular_table_name}_form_params" %>
       <%- if attributes_names.empty? -%>
       params.fetch(:<%= singular_table_name %>, {})
       <%- else -%>
